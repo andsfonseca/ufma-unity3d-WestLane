99e28ec80cc1a5888c2e5b0f8f57a682d62922aa
andsfonseca <andsf@live.com>
2017-01-01 21:28:15 -0300

    [Changelog]
    [ADD] Criado Classes SceneObject e SceneGenerator que são responsáveis
    pelo Geraão do Cenário
    [ADD] Adicionado GameLogic

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+
+	/// <summary>
+	/// Gets the instance.
+	/// </summary>
+	/// <value>The instance.</value>
+	public static GameLogic Instance
+	{
+		get
+		{
+			if (m_instance == null)
+			{
+				m_instance = GameObject.FindObjectOfType<GameLogic>();
+				DontDestroyOnLoad(m_instance.gameObject);
+			}
+
+			return m_instance;
+		}
+	}
+
+	/// <summary>
+	/// The m instance.
+	/// </summary>
+	private static GameLogic m_instance;
+	void Awake()
+	{
+		if (m_instance == null)
+		{
+			m_instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			if (this != m_instance)
+			{
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SceneGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneGenerator : MonoBehaviour {
+
+	[Header("Phases")]
+	public SceneObject[] Phases;
+
+	[Header("Initial Phase")]
+	public SceneObject InitialPhase;
+
+	[Header("LineBlock")]
+	public GameObject LineBlocks;
+
+	public string PhaseProgression;
+
+	private List<SceneObject> m_PhaseProgressionList;
+	private List<GameObject> m_LineBlocks;
+	private SceneObject m_currentPhase;
+	private int m_currentLine;
+	private bool m_isRandom;
+
+    ///<summary>
+    ///Recupera a fase atual
+    ///</summary>
+    public SceneObject CurrentPhase{
+		get{
+			return m_currentPhase;
+		}
+	}
+
+    ///<summary>
+    ///Recupera a linha da fase atual
+    ///</summary>
+	public int CurrentLine{
+		get{
+			return m_currentLine;
+		}
+	}
+
+    /// <summary>
+    /// Gera um cenário inicial com o tamanho delimitado pelas linhas
+    /// </summary>
+    /// <param name="lines">Numero de linhas a ser instaciado</param>
+	public void StartGeneration(int lines){
+		
+		if (PhaseProgression == "random") {
+			m_isRandom = true;
+		} 
+		else {
+			m_PhaseProgressionList = new List<SceneObject> ();
+            foreach (string s in PhaseProgression.Split(',')) {
+				m_PhaseProgressionList.Add (Phases [int.Parse (s)]);
+			}
+				
+		}
+		m_currentLine = 0;
+		m_currentPhase = InitialPhase;
+
+		for (int i = 1; i > lines; i++) {
+			GenerateLine ();
+		}
+	}
+
+    /// <summary>
+    /// Gera uma linha do cenário
+    /// </summary>
+	public void GenerateLine(){
+		//TODO: Definir NovaPosição
+		Vector3 nextPosition = new Vector3(0,0,0);
+
+		foreach (GameObject go in m_LineBlocks) {
+			if (!go.activeSelf) {
+				go.SetActive (true);
+
+				//TODO: Ajustar a nova posição
+				//TODO: Gerar Sprites
+				return;
+			}
+		}
+
+		GameObject lb = Instantiate (LineBlocks, nextPosition , Quaternion.identity);
+		//TODO: Gerar Sprites
+	}
+
+
+}
