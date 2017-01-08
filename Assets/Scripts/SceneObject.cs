72959168ff219c52782cb149253d958685b9d76a
andsfonseca <andsf@live.com>
2017-01-08 13:07:54 -0300

    [Changelog]
    [ADD] Adicionado ScenePreview
    [ADD] Adicionado Recurso de adicionar objetos aos Tiles
    [MOD] Bug Fixes

--- Assets/Scripts/SceneObject.cs
+++ Assets/Scripts/SceneObject.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEditor;
 
 public class SceneObject : MonoBehaviour {
 
@@ -12,12 +13,19 @@ public class SceneObject : MonoBehaviour {
     public Sprite[] sprites;
 
 	private int[][] m_matriz = null;
-	//public string[] osbstaculo;
-	//public char especial;
-	//public string[] hole;
+
+    [HideInInspector]
+    [SerializeField]
+    private List<GameObject> m_customizedElements;
+    [HideInInspector]
+    [SerializeField]
+    private List<int> m_customizedElementsComplement;
+    //public string[] osbstaculo;
+    //public char especial;
+    //public string[] hole;
 
 
-	public void generateMatriz(){
+    public void generateMatriz(){
 		//contador para saber o quanto ja percorri
 		int count = 0;
 		//matriz com o numero de linhas (altura)
@@ -55,6 +63,25 @@ public class SceneObject : MonoBehaviour {
         }
         return null;
 	}
+
+    public GameObject getGameobject(int line, int column)
+    {
+        if (m_matriz == null) generateMatriz();
+        if (line < m_matriz.Length)
+        {
+            if (column < m_matriz[line].Length)
+            {
+                int number = m_matriz[line][column];
+                if (number < sprites.Length)
+                {
+                    if (m_customizedElementsComplement.Contains(number)) {
+                        return m_customizedElements[m_customizedElementsComplement.IndexOf(number)];
+                    }
+                }
+            }
+        }
+        return null;
+    }
 		
 	public bool OnPlayerTryTouch(int line,  int column){
 
@@ -68,4 +95,59 @@ public class SceneObject : MonoBehaviour {
 			return true;
 		}
 	}
+
+    [CustomEditor(typeof(SceneObject))]
+    public class SceneObjectEditor : Editor
+    {
+        bool showAllGameObjects = false;
+        bool showCreateGameObject = false;
+
+        int tile;
+        GameObject go;
+        public override void OnInspectorGUI()
+        {
+            SceneObject so = (SceneObject)target;
+
+            DrawDefaultInspector();
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Tiles With Game Objects", EditorStyles.boldLabel);
+            showAllGameObjects = EditorGUILayout.Foldout(showAllGameObjects, "Show All");
+            if (showAllGameObjects)
+            {
+                for (int i = 0; i < so.m_customizedElements.Count; i++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Label("Tile: " + so.m_customizedElementsComplement[i]);
+                    so.m_customizedElements[i] = EditorGUILayout.ObjectField(so.m_customizedElements[i], typeof(GameObject), true) as GameObject;
+                    if (GUILayout.Button("Remove")) {
+                        so.m_customizedElements.RemoveAt(i);
+                        so.m_customizedElementsComplement.RemoveAt(i);
+                        Debug.Log("GameObject removido do Tile");
+                        Repaint();
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+            showCreateGameObject = EditorGUILayout.Foldout(showCreateGameObject, "Create New");
+            if (showCreateGameObject)
+            {
+                
+                tile = EditorGUILayout.IntField("Tile Number", tile);
+                go = EditorGUILayout.ObjectField("GameObject",go, typeof(GameObject), true) as GameObject;
+                if (GUILayout.Button("Add GameObject")) {
+                    if (tile < so.sprites.Length)
+                    {
+                        so.m_customizedElements.Add(go);
+                        so.m_customizedElementsComplement.Add(tile);
+                        Debug.Log("GameObject associado ao Tile");
+                        //Repaint();
+                    }
+                    else {
+                        Debug.Log("Tile Inexistente");
+                    }
+                }
+            }
+        }
+
+    }
 }
