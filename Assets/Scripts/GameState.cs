d74f7dba3abaca6203966b63cc6de7c3e0bb541a
andsfonseca <andsf@live.com>
2017-01-10 12:41:26 -0300

    [Changelog]
    [ADD] Instância de menu funcional
    [MOD] Gamelogic agora conhece os estados

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameState {
+
+    protected bool m_isEnabled;
+    protected EGameState m_gamestate;
+
+    public EGameState name { get { return m_gamestate; } }
+
+    public abstract void UpdateState();
+
+    public virtual void OnStartGameState() {
+        m_isEnabled = true;
+    }
+    public virtual void OnChangeGameState() {
+        m_isEnabled = false;
+    }
+
+    public GameState(EGameState idGameState){
+        m_gamestate = idGameState;
+    }
+}
---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour{
+
+    private GameState m_currentGameState;
+    private GameState m_lastGameState;
+
+    public EGameState current {
+        get {
+            return m_currentGameState.name;
+        }
+    }
+
+    public EGameState last {
+        get {
+            return m_lastGameState.name;
+        }
+    }
+
+    public void SwitchGameState(GameState gs) {
+        if (m_currentGameState != null) m_lastGameState = m_currentGameState;
+        if (m_currentGameState != null) m_currentGameState.OnChangeGameState();
+        m_currentGameState = gs;
+        m_currentGameState.OnStartGameState();
+    }
+
+    void Update() {
+        if (m_currentGameState != null) m_currentGameState.UpdateState();
+    }
+
+
+    public void BtnPlay() {
+        SwitchGameState(new GameplayState());
+    }
+
+}
+
