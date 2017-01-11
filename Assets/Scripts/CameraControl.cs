1cfe611929ce93de886eca28c6e9c116c7bbbab6
welton marinho <welton457@gmail.com>
2017-01-11 17:35:23 -0300

    [log] adicionei as animações []

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour {
 	private GameObject p1;
 	private GameObject p2;
-	private Vector3 offset;
+	//private Vector3 offset;
 	private float x;
 	private float y;
 	// Use this for initialization
 	void Start () {
 		p1 = GameLogic.Instance.player1.gameObject;
 		p2 = GameLogic.Instance.player2.gameObject;
-		offset = new Vector3 ((p1.transform.position.x - p2.transform.position.x) - this.transform.position.x, p1.transform.position.y - this.transform.position.y);
+		//offset = new Vector3 ((p1.transform.position.x - p2.transform.position.x) - this.transform.position.x, p1.transform.position.y - this.transform.position.y);
 	}
 	
 	// Update is called once per frame
